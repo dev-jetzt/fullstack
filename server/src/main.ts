955698e358dd6ee653f82d5866d34e0d787cb68d
Pascal Ludwig <1093680+Pasukaru@users.noreply.github.com>
2020-03-22 21:37:36 +0100

    Added example for SQLite Signed-off-by: Pascal Ludwig <1093680+Pasukaru@users.noreply.github.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 import { initExpress } from './initExpress';
-import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const server = await initExpress();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors();
-  await app.listen(process.env.PORT as string);
+  await app.listen(3000);
 }
 bootstrap();
