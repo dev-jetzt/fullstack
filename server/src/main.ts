cbb8a24c24d5718a6dd2bdd8ed7b3254797a1243
Joshua Jung <mail@joshua-jung.com>
2020-03-23 13:10:52 +0100

    Cont

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const server = await initExpress();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT as string);
 }
 bootstrap();
